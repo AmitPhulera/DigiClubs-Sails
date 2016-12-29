c509f80dfa34812944dff75f3a41f57360bf9ce9
Amit Phulera <amitphulera>
2016-12-29 11:56:59 +0530

    notification work under progress

--- Digiclubs/src/js/custom.js
+++ Digiclubs/src/js/custom.js
@@ -10,4 +10,38 @@ $( document ).ready(function(){
 document.addEventListener('deviceready', function(){
     // Change the color
     window.plugins.headerColor.tint("#3f51b5");
+    var push = PushNotification.init({
+            android: {
+                senderID: "565734768883"
+            },
+            browser: {
+                pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+            },
+            ios: {
+                alert: "true",
+                badge: "true",
+                sound: "true"
+            },
+            windows: {}
+        });
+
+        push.on('registration', function(data) {
+            // data.registrationId
+        });
+
+        push.on('notification', function(data) {
+            // data.message,
+            // data.title,
+            // data.count,
+            // data.sound,
+            // data.image,
+            // data.additionalData
+            alert(data.message);
+        });
+
+        push.on('error', function(e) {
+            // e.message
+            alert(e.message);
+        });
+
 }, false);
